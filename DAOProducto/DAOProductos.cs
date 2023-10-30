1a6594af9caa664a9b15a713abfaded633895243
Antonio Castro <72706037+antonio-1983@users.noreply.github.com>
2023-10-30 10:39:13 -0300

    biblioteca DAO de productos
    se agrego la biblioteca para la dependencia del programa del kiosco

---
+++ DAOProducto/DAOProductos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Dapper;
+using MySqlX.XDevAPI.Common;
+
+
+
+namespace DAOProducto 
+{
+    public class DAOProductos : IDAOProductos
+    {
+        string connection = @"Server=localhost; Database=ejercicioKiosco; Uid=root;";
+        string sqlProducto = "select Id,Nombre,Precio,Alcohol,ParaMayorEdad,Stock from Productos;";
+        string sqlId = "select Id,Nombre,Precio,Alcohol,ParaMayorEdad,Stock from Productos where Id = @Id;";
+        string sqlUpdate = "update Productos set Stock = @Stock where Id = @Id;";
+        public DAOProductos() { }
+
+        public List<Producto> ObtenerProductos()
+        {
+            using (var db = new MySqlConnection(connection))
+            {
+                List<Producto> result = new List<Producto>();
+                result = (List<Producto>)db.Query<Producto>(sqlProducto);
+                return result;
+            }
+
+        }
+
+        public Producto ObtenerProducto(int id)
+        {
+            using (var db = new MySqlConnection(connection))
+            {
+                Producto result = new Producto();
+                result = db.QueryFirstOrDefault<Producto>(sqlId, new { Id = id });
+                return result;
+            }
+        }
+
+        public void ActualizarProducto(int id,int stock)
+        {
+            using (var db = new MySqlConnection(connection))
+            {
+
+                var result = db.Execute(sqlUpdate, new { stock,id });
+
+            }
+
+        }
+    }
+}
---
+++ DAOProducto/Producto.cs
@@ -0,0 +1,12 @@
+﻿namespace DAOProducto
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Precio { get; set; }
+        public string Alcohol { get; set; }
+        public string ParaMayorEdad { get; set; }
+        public int Stock { get; set; }
+    }
+}
