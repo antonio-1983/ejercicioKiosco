14322d1f47ea5e19d5876a400a0e6c9c23b5bbc5
Antonio Castro <72706037+antonio-1983@users.noreply.github.com>
2023-11-03 20:07:44 -0300

    cambio de tipo de dato
    Se cambio el tipo de dato de la variable Alcohol y ParaMayores

--- DAOProducto/Producto.cs
+++ DAOProducto/Producto.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public string Nombre { get; set; }
         public int Precio { get; set; }
-        public string Alcohol { get; set; }
-        public string ParaMayorEdad { get; set; }
+        public bool Alcohol { get; set; }
+        public bool ParaMayorEdad { get; set; }
         public int Stock { get; set; }
     }
 }
